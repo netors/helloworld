2ab783e0448705b8156a5e2b74d87e6a1d7682d9
Ernesto Ruy Sanchez <ernesto@tmake.mx>
2017-04-17 19:09:08 -0700

    pass tests

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,8 @@
 var test = require('unit.js');
 var str = 'hello world';
 
-test.string(str).startsWith('Hello');
+test.string(str).startsWith('hello');
 
-if (test.string(str).startsWith('Hello')) {
+if (test.string(str).startsWith('hello')) {
   console.log('Passed');
 }
